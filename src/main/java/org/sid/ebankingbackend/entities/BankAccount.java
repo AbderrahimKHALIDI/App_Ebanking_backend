68d7c5c85c36eceb02ee7d9124323858f8e10c95
Skramblo <97240716+Skramblo@users.noreply.github.com>
2023-05-18 01:22:47 +0200

    fix

--- src/main/java/org/sid/ebankingbackend/entities/BankAccount.java
+++ src/main/java/org/sid/ebankingbackend/entities/BankAccount.java
@@ -12,14 +12,15 @@ import java.util.List;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "TYPE",length = 4)
 @Data @AllArgsConstructor @NoArgsConstructor
-public class BankAccount {
+public abstract class BankAccount {
     @Id
     private String id;
     private double balance;
     private Date createdAt;
+    @Enumerated(EnumType.STRING)
     private AccountStatus status;
     @ManyToOne
     private Customer customer;
-@OneToMany(mappedBy = "bankAccount")
+@OneToMany(mappedBy = "bankAccount" , fetch = FetchType.EAGER)
     private List<AccountOperation> accountOperations;
 }
