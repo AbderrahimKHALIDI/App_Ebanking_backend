61f1c967eec27614deb3889bf602a9481fcdc7fc
Skramblo <97240716+Skramblo@users.noreply.github.com>
2023-05-17 02:33:36 +0200

    first commit

---
+++ src/main/java/org/sid/ebankingbackend/entities/BankAccount.java
@@ -0,0 +1,26 @@
+package org.sid.ebankingbackend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.ebankingbackend.enums.AccountStatus;
+
+import java.util.Date;
+import java.util.List;
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor
+public class BankAccount {
+    @Id
+    private String id;
+    private double balance;
+    private Date createdAt;
+    private AccountStatus status;
+    @ManyToOne
+    private Customer customer;
+@OneToMany(mappedBy = "bankAccount")
+    private List<AccountOperation> accountOperations;
+}
